7102e68b8f39cdb09be270554625f9b49e6edb9f
Katsumasa-Sato <katsumasa-sato@outlook.com>
2021-08-19 10:13:58 +0900

    put database

--- utils/errors/rest_error.go
+++ utils/errors/rest_error.go
@@ -3,30 +3,31 @@ package errors
 import "net/http"
 
 type RestErr struct {
-  Message string `json:"message"`
-  Status  int    `json:"code"`
-  Error   string `json:"error"`
+	Message string `json:"message"`
+	Status  int    `json:"code"`
+	Error   string `json:"error"`
 }
 
-func NewBadRequestError(message string)*RestErr {
-  return &RestErr{
-    Message: message,
-    Status:  http.StatusBadRequest,
-    Error:   "bad_request",
-  }
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusBadRequest,
+		Error:   "bad_request",
+	}
 }
 
-func NewNotFoundError(message string)*RestErr {
-  return &RestErr{
-    Message: message,
-    Status:  http.StatusNotFound,
-    Error:   "not_found",
-  }
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusNotFound,
+		Error:   "not_found",
+	}
 }
 
-func NewInternalServerError(message string) *RestErr {  return &RestErr{
-  Message: message,
-  Status:  http.StatusInternalServerError,
-  Error:   "Internal server error",
-}
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusInternalServerError,
+		Error:   "Internal server error",
+	}
 }
